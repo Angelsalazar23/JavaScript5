6009dbbe41d8f63ecdf915dc89206e341db5f323
Angelsalazar23 <angelitosalazar230597@gmail.com>
2022-10-11 15:50:20 -0500

    Terminado

---
+++ numeros.js
@@ -0,0 +1,30 @@
+let alturacm = 173;
+console.log(alturacm)
+let alturaM=1.73;
+console.log(alturaM)
+let peso=82.1;
+console.log(peso)
+
+let alturRedup= Math.ceil(alturaM)
+console.log(alturRedup)
+
+let pesoReddown=Math.floor(peso)
+console.log(pesoReddown)
+
+
+
+let aux=false;
+let max1=Number.MAX_VALUE + 1 ;
+
+
+let max=Number.MAX_VALUE;
+
+if(max1===max){
+
+    aux=true
+}else{
+
+    aux=false
+}
+
+console.log(aux)
